d91a7a257109db7614476fcf478755f047b26195
DiegoACA <a01656065@itesm.mx>
2021-08-24 15:05:02 -0500

    update

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -5,33 +5,36 @@ def main():
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
 
-    if x > y and x > z:
-        if y > z:
-            print(z)
-            print(y)
-            print(x)
-        else:
-            print(y)
-            print(z)
-            print(x)
-    elif y > x and y > z:
-        if x > z:
-            print(z)
-            print(x)
-            print(y)
-        else:
-            print(x)
-            print(z)
-            print(y)
-    elif z > y and z > x:
-        if y > x:
-            print(x)
-            print(y)
-            print(z)
-        else:
-            print(y)
-            print(x)
-            print(z)
+    if x != y and x != z and z != y:
+        if x > y and x > z:
+            if y > z:
+                print(z)
+                print(y)
+                print(x)
+            else:
+                print(y)
+                print(z)
+                print(x)
+        elif y > x and y > z:
+            if x > z:
+                print(z)
+                print(x)
+                print(y)
+            else:
+                print(x)
+                print(z)
+                print(y)
+        elif z > y and z > x:
+            if y > x:
+                print(x)
+                print(y)
+                print(z)
+            else:
+                print(y)
+                print(x)
+                print(z)
+    else:
+        print("nùmeros no validos")
 
     pass
 
