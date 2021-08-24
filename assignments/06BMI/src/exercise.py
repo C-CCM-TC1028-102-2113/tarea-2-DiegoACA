4ec776e97ecabd31c5f38843a37a800433e42d6e
DiegoACA <a01656065@itesm.mx>
2021-08-24 14:47:18 -0500

    update

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,21 +1,25 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    p = float(input("Peso?"))
-    a = float(input("Altura?"))
+    p = float(input('Peso en kg: '))
+    a = float(input('Altura en m: '))
 
     bmi = p / a**2
 
-    if bmi < 20:
-        print("peso bajo")
-    elif 20 <= bmi < 25:
-        print("normal")
-    elif 25 <= bmi < 30:
-        print("sobrepeso")
-    elif 30 <= bmi < 40:
-        print("obesidad")
-    elif bmi >= 40:
-        print("obesidad morbida")
+    if p < 0 or a < 0:
+        print('Revisa tus datos, alguno de ellos es erróneo.')
+
+    else:
+        if bmi < 20:
+            print("peso bajo")
+        elif 20 <= bmi < 25:
+            print("normal")
+        elif 25 <= bmi < 30:
+            print("sobrepeso")
+        elif 30 <= bmi < 40:
+            print("obesidad")
+        elif bmi >= 40:
+            print("obesidad morbida")
 
     pass
 if __name__=='__main__':
