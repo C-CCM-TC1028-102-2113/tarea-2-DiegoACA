897e7eeb5f4b9816c3efbd1d3594b25c50a31578
DiegoACA <a01656065@itesm.mx>
2021-08-24 14:36:27 -0500

    update

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -2,7 +2,7 @@ import math
 def main():
     #escribe tu código abajo de esta línea
 
-    num = int(input("Dame un nùmero entero:"))
+    num = int(input('Dame un número: '))
 
     if num >= 0:
         print("es positivo")
